745a90f888581ab2b02fb7a6c7b07a5f734ea7c8
James Min <jmin499@gmail.com>
2020-10-02 03:05:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,5 +10,6 @@ document.body.appendChild(element);
 let ul = document.createElement('ul');
 for(let i = 0; i < 3; i++) {
   let li = document.createElemen('li');
-  li.innerHTML = (i + )
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(ul)
 }
