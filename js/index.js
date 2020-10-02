60f61827af8786ff7b39caa32310f9ede21e98b2
James Min <jmin499@gmail.com>
2020-10-02 03:06:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ element.style.textAlign = 'center'
 
 let ul = document.createElement('ul');
 for(let i = 0; i < 3; i++) {
-  let li = document.createElemen('li');
+  let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li)
 }
