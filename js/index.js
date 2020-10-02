c32b7a732e765a3a41fe22b146638a25b54fbc04
James Min <jmin499@gmail.com>
2020-10-02 03:03:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,5 +3,6 @@
 let element = document.createElement('div')
 element.innerHTML = 'Hello DOM!'
 element.style.backgroundColor = '#f9f9f9';
+element.style.textAlign = 'center'
 
 document.body.appendChild(element);
