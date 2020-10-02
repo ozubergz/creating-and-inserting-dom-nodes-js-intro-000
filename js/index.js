493b08f5be75fd9b82067d53b63fe8f04cb42eb2
James Min <jmin499@gmail.com>
2020-10-02 03:02:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@
 //you can use this to paste in js code and have it run when index.html is loaded
 let element = document.createElement('div')
 element.innerHTML = 'Hello DOM!'
-element.style.backgroundColor =
+element.style.backgroundColor = '#f9f9f9';
